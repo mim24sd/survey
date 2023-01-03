af998372b1ff16fbae67ea60d7456021ce3955a7
Shamim <mim24sd@gmail.com>
2023-01-03 19:16:58 +0330

    fix sass config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CSSPlugin = require("@modular-css/webpack/plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const isDevelopment = process.env.NODE_ENV === "development";
 
 module.exports = {
   entry: path.join(__dirname, "src", "App.jsx"),
@@ -22,49 +19,8 @@ module.exports = {
         },
       },
       {
-        test: /\.((s*)css|sass)$/i,
+        test: /\.(scss)$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /.css$/,
-        use: "@modular-css/webpack/loader",
-      },
-      {
-        test: /\.module\.s(a|c)ss$/,
-        loader: [
-          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              sourceMap: isDevelopment,
-            },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        exclude: /\.module.(s(a|c)ss)$/,
-        loader: [
-          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
       },
     ],
   },
@@ -72,16 +28,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "index.html"),
     }),
-    new CSSPlugin({
-      css: "./output.css",
-      json: "./output.json",
-    }),
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
-      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
-    }),
   ],
-  resolve: {
-    extensions: [".js", ".jsx", ".scss"],
-  },
+  mode: "development",
 };
